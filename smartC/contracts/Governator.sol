53a9076d4dcc96a735c7dedacf625c3f340b0c26
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2024-08-31 09:24:51 +0100

    all set... up next, saniy checks

--- smartC/contracts/Governator.sol
+++ smartC/contracts/Governator.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
 import "./inherit/NFT.sol";
-import "./storage.sol";
+import "./inherit/storage.sol";
 
 contract Governator is Storage,  Governator_NFT(msg.sender) {
 
